5fc2130df236d40af7a295c1e3c6f31bc23b7783
Murilo Nascimento <murilojesus659@gmail.com>
2022-10-18 16:27:55 -0300

    Exercício Crud em TS

---
+++ src/produto/entities/produto.entity.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty } from "class-validator";
+import { Categoria } from "src/categoria/entities/categoria.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity({name: "tb_produto"})
+export class Produto {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @IsNotEmpty()
+    @Column({length: 100, nullable: false})
+    nome: string
+
+    @ManyToOne(() => Categoria, (categoria) => categoria.produto, {
+        onDelete: "CASCADE"
+    })
+    categoria: Categoria
+}
